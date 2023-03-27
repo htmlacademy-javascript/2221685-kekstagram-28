8a62cdefaaa498d351dac1d986fdea023e735d93
Nika Buzynska <nikabuzynska@gmail.com>
2023-03-27 10:52:37 +0100

    validate form

---
+++ js/form.js
@@ -0,0 +1,71 @@
+import { isEscapeKey } from './util.js';
+const sectionPictures = document.querySelector('.pictures');
+const bodyElement = document.querySelector('body');
+const form = sectionPictures.querySelector('.img-upload__form');
+const uploadFile = form.querySelector('#upload-file');
+const imgUpload = form.querySelector('.img-upload__overlay');
+const uploadCancelButton = form.querySelector('#upload-cancel');
+const commentField = form.querySelector('.text__description');
+
+const pristine = new Pristine(form, {
+  classTo: 'img-upload__text',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__text-error',
+});
+
+const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+
+const inputHashtag = document.querySelector('.text__hashtags');
+
+pristine.addValidator(inputHashtag, (value) => {
+  if (hashtag.test(value)) {
+    console.log('Hashtag test true');
+    return true;
+  } else {
+    console.log('Hashtag test false');
+    return false;
+  }
+
+}, 'Hashtag is not valid', 2, false);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  const isValid = pristine.validate();
+  if (isValid) {
+    console.log('Можно отправлять');
+  } else {
+    console.log('Форма невалидна');
+  }
+});
+
+
+pristine.addValidator(commentField, (value) => {
+  if (value.length > 140) {
+    return false;
+  }
+  return true;
+}, 'Комментарий не может быть длиннее 140 символов');
+
+uploadFile.addEventListener('change', () => {
+  imgUpload.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+});
+
+const uploadCancelButtonFunc = function (evt) {
+  if (document.activeElement === commentField) {
+    evt.preventDefault();
+  } else {
+    imgUpload.classList.add('hidden');
+    bodyElement.classList.remove('modal-open');
+  }
+};
+
+uploadCancelButton.addEventListener('click', () => {
+  uploadCancelButtonFunc();
+});
+
+document.addEventListener('keydown', (evt) => {
+  if (isEscapeKey(evt)) {
+    uploadCancelButtonFunc();
+  }
+});
--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ import { createPosts } from'./data.js';
 
 import { renderPhotos } from './pictures.js';
 
-
+import './form.js';
 const photoArray = createPosts();
 
 renderPhotos(photoArray);
