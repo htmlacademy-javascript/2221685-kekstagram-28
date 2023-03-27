434dc2df9bf5d6ef6d12d748a79dc50ba10a2f5c
Nika Buzynska <nikabuzynska@gmail.com>
2023-03-27 22:24:03 +0100

    refines the task

--- js/form.js
+++ js/form.js
@@ -6,6 +6,7 @@ const uploadFile = form.querySelector('#upload-file');
 const imgUpload = form.querySelector('.img-upload__overlay');
 const uploadCancelButton = form.querySelector('#upload-cancel');
 const commentField = form.querySelector('.text__description');
+const textHashtags = form.querySelector('.text__hashtags');
 
 const pristine = new Pristine(form, {
   classTo: 'img-upload__text',
@@ -13,12 +14,11 @@ const pristine = new Pristine(form, {
   errorTextClass: 'img-upload__text-error',
 });
 
-const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
 
 const inputHashtag = document.querySelector('.text__hashtags');
 
 pristine.addValidator(inputHashtag, (value) => {
-  if (hashtag.test(value)) {
+  if (validateHashtags(value)) {
     console.log('Hashtag test true');
     return true;
   } else {
@@ -38,13 +38,41 @@ form.addEventListener('submit', (evt) => {
   }
 });
 
+function validateHashtags(hashtags) {
+  const hashtagsArray = hashtags.trim().split(/\s+/);
+  if (hashtagsArray.length > 5) {
+    return false;
+  }
+  const hashtagCounts = {};
+  for (let i = 0; i < hashtagsArray.length; i++) {
+    const hash = hashtagsArray[i];
+    const hashtag = /^#[a-zа-яё0-9]{1,19}$/i;
+    console.log(1);
+    if (hash.length === 0) {
+      return false;
+    }
+    console.log(2);
+    if (!hashtag.test(hash)) {
+      return false;
+    }
+    console.log(3);
+    if (hashtagCounts[hash] !== undefined) {
+      return false;
+    }
+    console.log(4);
+    hashtagCounts[hash] = 1;
+  }
+  return true;
+}
+
+
 uploadFile.addEventListener('change', () => {
   imgUpload.classList.remove('hidden');
   bodyElement.classList.add('modal-open');
 });
 
 const uploadCancelButtonFunc = function (evt) {
-  if (document.activeElement === commentField) {
+  if (document.activeElement === commentField || document.activeElement === textHashtags) {
     evt.preventDefault();
   } else {
     imgUpload.classList.add('hidden');
@@ -62,3 +90,4 @@ document.addEventListener('keydown', (evt) => {
   }
 });
 
+export {validateHashtags};
